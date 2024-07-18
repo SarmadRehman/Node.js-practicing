1e5e2b46f265a39e09d9149dc617f8d8e4bdc0fc
Sarmad Rehman <sarmadr02@gmail.com>
2024-07-18 06:44:44 +0500

    successfully showing the html template on request hit

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -13,22 +13,37 @@ const url = require("url");
 // console.log("File written");
 
 // SERVER
-const data = fs.readFile(
-  `${__dirname}/dev-data/data.json`,
-  "utf-8",
-  (err, data) => {
-    const dataObj = JSON.parse(data);
-  }
+// SERVER
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
 );
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+  //overview
   if (pathName === "/" || pathName === "./overview") {
-    res.end("This is the Overview");
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(tempOverview);
+    //product
   } else if (pathName === "/product") {
     res.end("This is the Product");
+    //api
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+    //page not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
